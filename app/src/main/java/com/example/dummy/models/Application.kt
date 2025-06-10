49136d94a3dc6d6846b4bd9e3ba26ee6134ad0c7
theod <theod@PHOQUEYOU>
2025-06-10 11:41:27 +0200

    add appList

---
+++ app/src/main/java/com/example/dummy/models/Application.kt
@@ -0,0 +1,10 @@
+package com.example.dummy.models
+
+class Application(
+    var nomApplication: String,
+    var auteur: String,
+    var description: String,
+    var note: Double,
+    var telechargement: Int,
+    var icone: String
+)
